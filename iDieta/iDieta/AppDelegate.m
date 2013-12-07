287971b5f659883d4b2d1980adb252e255dad2ca
Rafael Duarte <rafaelduarte@RDMacBookPro.local>
2013-12-07 17:18:43 -0200

    ok

--- iDieta/iDieta/AppDelegate.m
+++ iDieta/iDieta/AppDelegate.m
@@ -14,6 +14,8 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    //teste
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
